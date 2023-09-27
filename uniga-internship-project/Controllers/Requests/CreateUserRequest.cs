85e3da96817876623095e60d8c71fd2595bc2512
Unknowguy <unknowguy2001@gmail.com>
2023-09-27 19:59:05 +0700

    refactor: remove request folder in controller

--- uniga-internship-project/Controllers/Requests/CreateUserRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uniga_internship_project.Controllers.Requests
-{
-    public class CreateUserRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
