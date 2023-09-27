323eb378841486b1e50aa5ad82c0d0a197d11610
Unknowguy <unknowguy2001@gmail.com>
2023-09-27 14:45:06 +0700

    chore: fix login and add create token for login

---
+++ uniga-internship-project/Controllers/Requests/AuthorizeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Pkcs;
+using uniga_internship_project.Services.AuthorizeSerivice;
+using uniga_internship_project.Services.AuthorizeSerivice.Requests;
+
+namespace uniga_internship_project.Controllers.Requests
+{
+    [Route("api/authorize/")]
+    [ApiController]
+    public class AuthorizeController : ControllerBase
+    {
+        private readonly IAuthorizeService _authorizeService;
+
+        public AuthorizeController(IAuthorizeService authorizeService)
+        {
+            _authorizeService = authorizeService;
+        }
+        [HttpPost("")]
+        public async Task<ActionResult<Users>> Login([FromBody] LoginRequest request)
+        {
+            var result = await _authorizeService.Login(request);
+            return Ok(result);
+        }
+    }
+}
--- uniga-internship-project/Services/AuthorizeSerivice/AuthorizeService.cs
+++ uniga-internship-project/Services/AuthorizeSerivice/AuthorizeService.cs
@@ -1,29 +1,39 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Org.BouncyCastle.Asn1.Ocsp;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
 using uniga_internship_project.Data;
 using uniga_internship_project.Services.AuthorizeSerivice.Requests;
-
+using Microsoft.IdentityModel.JsonWebTokens;
 namespace uniga_internship_project.Services.AuthorizeSerivice
 {
     public class AuthorizeService : IAuthorizeService
     {
         private readonly DataContext _dataContext;
+        private readonly IConfiguration _configuration;
 
-        public AuthorizeService(DataContext dataContext)
+        public AuthorizeService(DataContext dataContext, IConfiguration configuration)
         {
             _dataContext = dataContext;
+            _configuration = configuration;
         }
         public async Task<string> Login(LoginRequest request)
         {
-            var user = await _dataContext.Users.FindAsync(request.Email);
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
             if (user == null)
                 throw new Exception();
-            if (!BCrypt.Net.BCrypt.EnhancedVerify(request.Password, user.Password))
+            if (BCrypt.Net.BCrypt.EnhancedVerify(request.Password, user.Password))
             {
                 throw new Exception();
             }
             //token 
-            throw new NotImplementedException();
+            string token = TokenGenerate(user);
+            
+            return token;
         }
 
         public async Task<bool> Register(RegisterRequest request)
@@ -38,6 +48,24 @@ namespace uniga_internship_project.Services.AuthorizeSerivice
             await _dataContext.SaveChangesAsync();
             return true;
         }
+        private string TokenGenerate(Users user)
+        {
+            List<Claim> claims = new List<Claim> {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSetting:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(3),
+                signingCredentials: creds
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
     }
 }
