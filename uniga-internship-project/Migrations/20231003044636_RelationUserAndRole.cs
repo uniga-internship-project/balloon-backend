c892e81b3d9abc1457dfe2e0932639c07e5d67f7
DESKTOP-LPLQKH1\BEER <beer27143@gmail.com>
2023-10-03 11:51:15 +0700

    feat: add relation for role and user

---
+++ uniga-internship-project/Migrations/20231003044636_RelationUserAndRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uniga_internship_project.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationUserAndRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
