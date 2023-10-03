ef2210c6474af37a53da883a690861b9823b9731
Unknowguy <unknowguy2001@gmail.com>
2023-10-03 13:05:26 +0700

    fix : merge

---
+++ uniga-internship-project/Models/Position.cs
@@ -0,0 +1,9 @@
+﻿namespace uniga_internship_project.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }  
+    }
+}
